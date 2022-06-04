519f82ff244ea9ae9f25c056b14248d2a56a40f5
YAGI <macbook@MACBOOKs-MacBook-Air.local>
2022-06-04 19:50:12 +0100

    Riyu-thrift-store initial commit

---
+++ src/data/BodySuits.js
@@ -0,0 +1,108 @@
+
+import {v4 as uuidv4} from 'uuid'
+
+const data = [
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+      {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+]
+// 
+
+
+export default data
---
+++ src/data/Products.js
@@ -0,0 +1,111 @@
+
+import {v4 as uuidv4} from 'uuid'
+
+const data = [
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+
+    {
+        id: uuidv4(),
+        image: './images/thrift-displays/Blazer Dress.jpg',
+        title: 'Blazer Dress',
+        desc: 'lorem ipsum dolor sit amet jkrds hshkbbhd jnsnxifsj jnsinmbbjn lngndfijg nldnnjgjn',
+        category: 'New',
+        type: 'Office/Casual',
+        price: '10',
+    },
+]
+// ] {
+//   return (
+//     <div>Products</div>
+//   )
+
+
+export default data
